421d3544a72186d51c396d9c69aa0f313f7c4025
do-mandoo <67843356+do-mandoo@users.noreply.github.com>
2023-05-02 12:37:44 +0900

    추가 삭제 구현 수정중

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,51 +3,73 @@ import { useEffect, useState } from 'react';
 
 const Cart = () => {
   const [items, setItems] = useState([]);
-  // const [name, setName] = useState('');
 
   // 장바구니 아이템 불러오기
   useEffect(() => {
-    const getItem = async () => {
-      const res = await axios.get('http://localhost:5000/items');
-      const data = res.data;
-      // setItems(data)
-      console.log(data, 'data');
+    const fetchItem = async () => {
+      try {
+        const res = await axios.get('http://localhost:5000/getItems');
+        const data = res.data;
+        setItems(data);
+        console.log(data, 'data');
+      } catch (error) {
+        console.log(error, '아이템 불러오기 에러');
+      }
     };
-    getItem();
+    fetchItem();
   }, []);
 
   // 장바구니 아이템 추가 함수
-  const addItem = e => {
-    // console.log(e.target.previousElementSibling.textContent, 'hi');
+  const addItem = async e => {
     const newItem = e.target.previousElementSibling.textContent;
+
     // setItems([...items, newItem]);
     // console.log(items, 'items');
-    // // setName('');
-    axios
-      .post('http://localhost:5000/items', newItem)
-      .then(res => {
-        setItems([...items, res.data]);
-      })
-      .catch(err => {
-        console.log(err, '아이템 추가 에러');
-      });
+    // setName('');
+    //---
+    // axios
+    //   .post('http://localhost:5000/items', { name: newItem })
+    //   .then(res => {
+    //     setItems([...items, res.data]);
+    //   })
+    //   .catch(err => {
+    //     console.log(err, '아이템 추가 에러');
+    //   });
+    // const newItem = 'pat';
+
+    try {
+      const res = await axios.post('http://localhost:5000/postItems', { name: newItem });
+      console.log(res, 'res');
+      console.log(newItem);
+      setItems([...items, res.data]);
+      console.log(items, 'itemsssss');
+    } catch (error) {
+      console.log(error, '아이템 추가 에러');
+    }
   };
 
   // 장바구니 아이템 삭제 함수
-  const removeItem = index => {
-    const updatedItems = items.filter((item, i) => i !== index);
-    setItems(updatedItems);
+  const removeItem = async index => {
+    // const updatedItems = items.filter((item, i) => i !== index);
+    // setItems(updatedItems);
+    try {
+      await axios.delete(`http://localhost:5000/deleteItems/${index}`);
+      const updatedItems = [...items];
+      updatedItems.splice(index, 1);
+      setItems(updatedItems);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
     <div>
       <ul style={{ width: '200px' }}>
-        {items &&
-          items.map((item, index) => (
-            <li key={index}>
-              {item} <button onClick={() => removeItem(index)}>삭제</button>
-            </li>
-          ))}
+        {items.map((item, index) => (
+          <li key={index}>
+            {item.name} <button onClick={() => removeItem(item._id)}>삭제</button>
+          </li>
+        ))}
       </ul>
       <div style={{ display: 'flex' }}>
         <div
@@ -61,7 +83,7 @@ const Cart = () => {
           }}
         >
           <div style={{ fontSize: '40px', fontWeight: 'bold', color: '#fff', marginBottom: '5px' }}>
-            Clothes
+            fff
           </div>
           <button onClick={e => addItem(e)}>추가</button>
           {/* <button>삭제</button> */}
@@ -77,7 +99,7 @@ const Cart = () => {
           }}
         >
           <div style={{ fontSize: '40px', fontWeight: 'bold', color: '#fff', marginBottom: '5px' }}>
-            Pants
+            ggg
           </div>
           <button onClick={e => addItem(e)}>추가</button>
           {/* <button>삭제</button> */}
@@ -92,7 +114,7 @@ const Cart = () => {
           }}
         >
           <div style={{ fontSize: '40px', fontWeight: 'bold', color: '#fff', marginBottom: '5px' }}>
-            Hat
+            hhh
           </div>
           <button onClick={e => addItem(e)}>추가</button>
           {/* <button>삭제</button> */}
