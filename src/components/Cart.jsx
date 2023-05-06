9ca08f6361767f4c52254fa5a31ae617ff70fd4f
do-mandoo <67843356+do-mandoo@users.noreply.github.com>
2023-05-06 21:02:31 +0900

    수량 증가, 감소 및 중복 아이템 추가시 수량 증가 구현

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,32 +3,43 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 const Cart = () => {
-  const [items, setItems] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
+  // const [newItem, setNewItem] = useState({
+  //   name: '',
+  //   description: '',
+  //   // price: '',
+  // });
 
   // 장바구니 아이템 불러오기
   useEffect(() => {
-    const fetchItem = async () => {
-      try {
-        const res = await axios.get('http://localhost:5000/getItems');
-        const data = res.data;
-        setItems(data);
-        console.log(data, 'data');
-      } catch (error) {
-        console.log(error, '아이템 불러오기 에러');
-      }
-    };
     fetchItem();
   }, []);
+
+  const fetchItem = async () => {
+    try {
+      const res = await axios.get('http://localhost:5000/getItems');
+      const data = res.data;
+      setCartItems(data);
+      console.log(data, 'data');
+    } catch (error) {
+      console.log(error, '아이템 불러오기 에러');
+    }
+  };
+
+  // const handleInputChange = e => {
+  //   setNewItem({ ...newItem, [e.target.name]: e.target.value });
+  // };
 
   // 장바구니 아이템 추가 함수
   const addItem = async e => {
     const newItem = e.target.previousElementSibling.textContent;
+    console.log(newItem);
     try {
       const res = await axios.post('http://localhost:5000/postItems', { name: newItem });
       console.log(res, 'res');
-      console.log(newItem);
-      setItems([...items, res.data]);
-      console.log(items, 'itemsssss');
+      fetchItem();
+      setCartItems([...cartItems, res.data]);
+      console.log(cartItems, 'itemsssss');
     } catch (error) {
       console.log(error, '아이템 추가 에러');
     }
@@ -40,17 +51,38 @@ const Cart = () => {
       await axios.delete(`http://localhost:5000/deleteItems/${itemId}`);
 
       // 배열 복사
-      const updatedItems = [...items];
+      const updatedItems = [...cartItems];
       // 삭제할 아이템id의 index추출
       const indexFind = updatedItems.findIndex((item, i) => item._id === itemId);
       console.log(indexFind, 'indexFind');
       // 추출한 index로 배열의splice를하여 아이템 삭제
       updatedItems.splice(indexFind, 1);
       // 상태 업데이트
-      setItems(updatedItems);
-      console.log(items, 'items');
+      setCartItems(updatedItems);
+      console.log(cartItems, 'cartItems');
     } catch (err) {
       console.log(err);
+    }
+  };
+
+  // 수량 증가
+  const increaseNum = async itemId => {
+    console.log(itemId, 'increase e');
+    try {
+      await axios.put(`http://localhost:5000/increase/${itemId}`);
+      fetchItem();
+    } catch (error) {
+      console.log(error, '수량 증가 에러');
+    }
+  };
+
+  // 수량 감소
+  const decreaseNum = async itemId => {
+    try {
+      await axios.put(`http://localhost:5000/decrease/${itemId}`);
+      fetchItem();
+    } catch (error) {
+      console.log(error, '수량 감소 에러');
     }
   };
 
@@ -114,6 +146,7 @@ const Cart = () => {
             borderRadius: '10px',
             padding: '4px',
             width: '160px',
+            marginRight: '5px',
           }}
         >
           <Box sx={{ fontSize: '40px', fontWeight: 'bold', color: '#fff', marginBottom: '5px' }}>
@@ -131,10 +164,35 @@ const Cart = () => {
             추가
           </Button>
         </Box>
+        <Box
+          sx={{
+            backgroundColor: '#aaa',
+            // border: '5px solid skyblue',
+            borderRadius: '10px',
+            padding: '4px',
+            width: '160px',
+            marginRight: '5px',
+          }}
+        >
+          <Box sx={{ fontSize: '40px', fontWeight: 'bold', color: '#fff', marginBottom: '5px' }}>
+            WWW
+          </Box>
+          <Button
+            sx={{
+              fontWeight: 'bold',
+              fontSize: '18px',
+              color: '#000',
+              '&:hover': { color: 'blue' },
+            }}
+            onClick={e => addItem(e)}
+          >
+            추가
+          </Button>
+        </Box>
       </Box>
       {/* 리스트 영역 */}
       <Box sx={{ display: 'flex', flexFlow: 'column nowrap' }}>
-        {items.map((item, index) => (
+        {cartItems.map((item, index) => (
           <Box
             sx={{
               display: 'flex',
@@ -143,9 +201,14 @@ const Cart = () => {
             }}
             key={index}
           >
-            <Box sx={{ flex: 3 }}>{item.name}</Box>
+            <Box sx={{ flex: 3, bgcolor: 'pink' }}>{item.name}</Box>
+            <Box sx={{ display: 'flex', bgcolor: 'yellowgreen' }}>
+              <Button onClick={() => decreaseNum(item._id)}>감소</Button>
+              <Box>{item.quantity}</Box>
+              <Button onClick={() => increaseNum(item._id)}>증가</Button>
+            </Box>
             <Button
-              sx={{ flex: 1, '&:hover': { color: 'red' } }}
+              sx={{ flex: 1, bgcolor: 'skyblue', '&:hover': { color: 'red' } }}
               onClick={() => removeItem(item._id)}
             >
               삭제
